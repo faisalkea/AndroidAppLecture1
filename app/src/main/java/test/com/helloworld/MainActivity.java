ecc7615b10a8187ca150f65c0a53c89fbe4265c6
faisalkea <fafj@kea.dk>
2019-02-17 14:10:16 +0100

    Implemented Wifi func

--- app/src/main/java/test/com/helloworld/MainActivity.java
+++ app/src/main/java/test/com/helloworld/MainActivity.java
@@ -1,6 +1,9 @@
 package test.com.helloworld;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,8 +17,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static String TAG = "MainActivity";
 
-    private Button b1, b2, b3, b4, b5, b6, b7;
-    private TextView tv1, tv2, tv3;
+    private Button b1, b2, b3, b5, b6, b7;
+    private TextView tv1, tv2, tv3, tv4;
     private ProgressBar pb;
     private RatingBar rb;
 
@@ -36,18 +39,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 tv2.setText("Changed from Button 1");
                 tv2.setTextColor(Color.YELLOW);
-
-                tv1.setBackgroundColor(Color.GREEN);
-                tv1.setTextSize(20);
             }
         });
 
-        b2.setOnClickListener(this);
-        b3.setOnClickListener(this);
-        b5.setOnClickListener(this);
-        b6.setOnClickListener(this);
-        b7.setOnClickListener(this);
-
+        tv4.setText(getString(R.string.tv_3) + " " + isOnline());
+        tv4.setTextColor(Color.BLACK);
+        tv1.setText(getString(R.string.tv_1) + " " + checkNetworkConnection());
+        tv1.setTextColor(Color.BLACK);
     }
 
     /**
@@ -59,14 +57,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         b1 = findViewById(R.id.b1);
         b2 = findViewById(R.id.b2);
         b3 = findViewById(R.id.b3);
-        b4 = findViewById(R.id.b4);
         b5 = findViewById(R.id.b5);
         b6 = findViewById(R.id.b6);
         b7 = findViewById(R.id.b7);
 
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+
         tv1 = findViewById(R.id.tv1);
         tv2 = findViewById(R.id.tv2);
         tv3 = findViewById(R.id.tv3);
+        tv4 = findViewById(R.id.tv4);
 
         pb = findViewById(R.id.pb);
 
@@ -86,18 +90,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 tv2.setText("Changed from Button 2");
                 tv2.setTextColor(Color.RED);
-
-                tv1.setBackgroundColor(Color.YELLOW);
-                tv1.setTextSize(22);
                 break;
             case R.id.b3:
                 Log.d(TAG, "B3: onClick: called");
 
                 tv2.setText("Changed from Button 3");
                 tv2.setTextColor(Color.BLUE);
-
-                tv1.setBackgroundColor(Color.RED);
-                tv1.setTextSize(24);
                 break;
             case R.id.b5:
                 Log.d(TAG, "B5: onClick: called");
@@ -119,19 +117,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /**
      * Onclick method defined in xml view
+     * Doesn't need to be initialized
      * @param view
      */
     public void reset(View view) {
         Log.d(TAG, "reset() method called");
 
-        tv1.setText(getString(R.string.tv_1));
-        tv1.setBackgroundColor(Color.TRANSPARENT);
-        tv1.setTextColor(Color.GRAY);
-        tv1.setTextSize(14);
-
-        tv1.setText(getString(R.string.tv_2));
+        tv2.setText(getString(R.string.tv_2));
         tv2.setBackgroundColor(Color.TRANSPARENT);
         tv2.setTextColor(Color.GRAY);
         tv2.setTextSize(14);
+    }
+
+    private boolean isOnline() {
+        Log.d(TAG, "Checking for Internet Connectivity");
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private String checkNetworkConnection() {
+        Log.d(TAG, "Checking for Internet Connectivity");
+        String result = getString(R.string.no_wifi_or_mobile);
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeInfo = connMgr.getActiveNetworkInfo();
+
+        if (activeInfo != null && activeInfo.isConnected()) {
+            boolean wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
+            boolean mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+
+            if(wifiConnected) {
+                Log.i(TAG, getString(R.string.wifi_connection));
+                result =  getString(R.string.wifi_connection);
+            } else if (mobileConnected){
+                Log.i(TAG, getString(R.string.mobile_connection));
+                result = getString(R.string.mobile_connection);
+            }
+        }
+
+        Log.i(TAG, getString(R.string.no_wifi_or_mobile));
+        return result;
     }
 }
